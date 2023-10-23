22d1bd203af41b30eb8f8ecec10b32688fd6ba64
George Stoychev <georgi.stoychev.-nd@disney.com>
2023-10-23 18:31:19 +0300

    Fix API path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import:
-    "https://kzwox5c0hj.execute-api.eu-central-1.amazonaws.com/dev/import",
+  import: "https://kzwox5c0hj.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://lso9go55d0.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
