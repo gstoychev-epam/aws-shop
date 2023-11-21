6b169bd23cd61c78bb9dbfb8bbb08c6cabec2b87
George Stoychev <georgi.stoychev.-nd@disney.com>
2023-11-21 13:26:47 +0200

    changes related to task-8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://kzwox5c0hj.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://lso9go55d0.execute-api.eu-central-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://v0h8xzdxgd.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
